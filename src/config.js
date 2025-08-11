9bdb744014b0319973474b67308487b30bb66ebc
ylaher-prog <ylaher@qurtubaonline.co.za>
2025-08-11 14:19:10 +0200

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+// src/config.js
+export const POLL_MS = 60000;
+export const DEFAULT_SHEET_ID = "";
+export const DEFAULT_SHEETS = {
+  teachers: "Teachers",
+  subjects: "Subjects",
+  classes: "Classes",
+  allocation: "Allocation",
+  periods: "Periods",
+};
