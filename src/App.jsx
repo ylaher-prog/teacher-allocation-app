14d4ee8e08339f592ae921c4bf88260f8fdf71b1
ylaher-prog <ylaher@qurtubaonline.co.za>
2025-08-11 11:14:13 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function App(){
+  return (
+    <div style={{padding: 24}}>
+      <h1>Teacher Subject Allocation</h1>
+      <p>If you can see this, Vercel + Vite are building correctly.</p>
+    </div>
+  )
+}
